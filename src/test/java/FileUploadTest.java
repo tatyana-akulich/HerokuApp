949389dba73e7cd4a99e43388af6c64a690fa172
Tatyana Akulich <t.n.akulich@gmail.com>
2023-02-17 11:57:45 +0300

    HA_2_task_for_lecture9 - added tests

---
+++ src/test/java/FileUploadTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileUploadTest extends BaseTest {
+    @BeforeClass
+    public void openPage() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+    }
+
+    @Test
+    public void testUpload() {
+        String absolutePath = (System.getProperty("user.dir") + "/src/test/resources/testForUpload.docx")
+                .replaceAll("\\\\", "/");
+        driver.findElement(By.id("file-upload")).sendKeys(absolutePath);
+        driver.findElement(By.id("file-submit")).click();
+        assertThat(driver.findElement(By.id("uploaded-files")).getText())
+                .as("Name of file uploaded to the site should be equal to name in file system")
+                .isEqualTo("testForUpload.docx");
+    }
+}
---
+++ src/test/java/FrameTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FrameTest extends BaseTest {
+    @BeforeClass
+    public void openPage() {
+        driver.get("https://the-internet.herokuapp.com/frames");
+    }
+
+    @Test
+    public void testFrame() {
+        driver.findElement(By.xpath("//a[text()='iFrame']")).click();
+        driver.switchTo().frame("mce_0_ifr");
+        assertThat(driver.findElement(By.xpath("//p")).getText())
+                .as("Text is not equal to text in frame")
+                .isEqualTo("Your content goes here.");
+    }
+}
